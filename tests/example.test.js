24cf0cfd72c6b1343e3588281d842cbf343cba00
Himayat <himayatu99@gmail.com>
2025-08-09 23:05:03 +0500

    Add goBack and goForward functions

--- tests/example.test.js
+++ tests/example.test.js
@@ -3,16 +3,22 @@ const puppeteer = require('puppeteer')
 describe('My First puppeteer Test', () => {
     it('Should luch the browser', async () => {
         const browser = await puppeteer.launch({
-            headless: false, slowMo: 50, devtools: false
+            headless: false, slowMo: 150, devtools: false
 
         })
 
         const page = await browser.newPage()
         await page.goto('https://example.com/')
         await page.waitForSelector('h1')
-        await page.reload()
+        await page.goto('https://dev.to/')
+        await page.waitForSelector('.side-bar')
+        await page.goBack()
         await page.waitForSelector('h1')
+        await page.goForward()
+        await page.waitForSelector('.side-bar')
         await browser.close()
+
+
 
 
 
